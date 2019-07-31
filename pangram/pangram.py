7c698434db200e1ed8b0997b455f4271fa9c3a6e
Scibud <53430099+Scibud@users.noreply.github.com>
2019-07-31 07:39:06 +0300

    Update pangram.py

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,22 +1,9 @@
+import string
 def is_pangram(txt):
-    list(txt)
-    a="abcdefghijklmnopqrstuvwxyz"
-    list(a)
-    txt.lower()
-    i=j=p=0
-    
-    for i in len(a):
-        for j in len(txt):
-            if a[i] is txt[j]:
-                p +=1
-                break
-                
-            else :
-                p-=1
-                break
-    if p >= 26 :
-        return True
-    else:
-        return False
-                
-
+    txt=str.lower(txt)
+    txt=list(txt)
+    s=list(string.ascii_lowercase)
+    for i in range(0,25):
+        if s[i] not in txt:
+            return False
+    return True
