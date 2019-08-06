cb50d7ddf3fcd5850ee495d4b44bde301fb4739b
Scibud <53430099+Scibud@users.noreply.github.com>
2019-08-06 12:16:23 +0300

    Solved

---
+++ matrix/matrix.py
@@ -0,0 +1,13 @@
+class Matrix(object):
+    def __init__(self, matrix_string):
+        
+        rows = matrix_string.splitlines()
+        self.mat = [[int(i) for i in row.split(' ')] for row in rows]       
+
+    def row(self, index):
+        return self.mat[index-1][:]
+        
+
+    def column(self, index):
+        mat_trans = [[self.mat[j][i] for j in range(len(self.mat))] for i in range(len(self.mat[0]))]
+        return mat_trans[index-1][:]
