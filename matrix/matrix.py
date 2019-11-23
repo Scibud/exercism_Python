466bc8243ddd269fdf7b4e54b9aa73868f54a032
Scibud <53430099+Scibud@users.noreply.github.com>
2019-11-23 20:31:19 +0300

    Iteration 2
    Column retreving done without transposing

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -8,6 +8,9 @@ class Matrix(object):
         return self.mat[index-1][:]
         
 
-    def column(self, index):
+    '''def column(self, index):
         mat_trans = [[self.mat[j][i] for j in range(len(self.mat))] for i in range(len(self.mat[0]))]
         return mat_trans[index-1][:]
+    '''
+    def column(self,index):
+        return [self.mat[i][index-1] for i in range(len(self.mat))]
